eedd4e8a1343ca38dd16174588f46a035014d010
anphastar2010 <anphastar2010@gmail.com>
2025-12-29 16:26:31 +0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -20,3 +20,33 @@ const songs = [
 `
   }
 ];
+{
+  id: 1767000271760,
+  title: "Yên vui một đời",
+  artist: "Nguyễn Mộng Huỳnh",
+  key: "C",
+  content: `
+1. [C] Chúa sẽ ban ngày nắng tiếp sau ngày [Dm] mưa
+[Fm] Chúa sẽ cho trời [G] sáng xóa đêm buồn [C] lo
+[Am] Nếu mắt môi mặn [C] đắng Chúa lau lệ [Dm] khô
+[Fm] Hỡi những ai nặng [G] gánh đến bên Ngài [C] chưa.
+
+ĐK:
+[F] Chúa là nguồn nước [C] trong cho ta nghỉ [Em] ngơi
+[Dm] Xóa lao [G] nhọc của tháng năm ngược [C] xuôi
+[F] Hỡi bao người đời [C] sống luôn nổi [Em] trôi
+[Dm] Đến bên [G] Ngài rồi sẽ yên vui một [C] đời.
+
+2. [C] Chúa sẽ cho cày sới đất hoang chờ [Dm] mưa
+[Fm] Chúa sẽ ban dòng [G] suối tưới sa mạc [C] trơ
+[Am] Chúa sẽ luôn đổi [C] mới trái tim cằn [Dm] khô
+[Fm] Hỡi những ai lạc [G] lối đã quay về [C] chưa.
+
+ĐK:
+[F] Chúa là nguồn nước [C] trong cho ta nghỉ [Em] ngơi
+[Dm] Xóa lao [G] nhọc của tháng năm ngược [C] xuôi
+[F] Hỡi bao người đời [C] sống luôn nổi [Em] trôi
+[Dm] Đến bên [G] Ngài rồi sẽ yên vui một [C] đời.
+  `
+}
+];
