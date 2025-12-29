9471d3bed55c451f5e89960e707e3bef0fb5318c
anphastar2010 <anphastar2010@gmail.com>
2025-12-29 16:27:14 +0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,27 +1,6 @@
 const songs = [
-  {
-    id: 1,
-    title: "Thánh thay quyền năng",
-    artist: "Thánh ca",
-    key: "C",
-    content: `
-[C]Thánh thay [G]quyền năng
-[Am]Danh Ngài cao [F]sang
-`
-  },
-  {
-    id: 2,
-    title: "Ngài là sức mạnh con",
-    artist: "Thờ phượng",
-    key: "D",
-    content: `
-[D]Ngài là [A]sức mạnh con
-[Bm]Con tin cậy nơi [G]Ngài
-`
-  }
-];
-{
-  id: 1767000271760,
+
+  id: 1,
   title: "Yên vui một đời",
   artist: "Nguyễn Mộng Huỳnh",
   key: "C",
