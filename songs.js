fbea5b4cf1dcc04be6f3076d2f7694bc4d434b10
anphastar2010 <anphastar2010@gmail.com>
2025-12-29 16:51:29 +0700

    Add files via upload

--- songs.js
+++ songs.js
@@ -21,12 +21,44 @@ const songs = [
 [Fm] Chúa sẽ ban dòng [G] suối tưới sa mạc [C] trơ
 [Am] Chúa sẽ luôn đổi [C] mới trái tim cằn [Dm] khô
 [Fm] Hỡi những ai lạc [G] lối đã quay về [C] chưa.
+  `
+},
 
-ĐK:
-[F] Chúa là nguồn nước [C] trong cho ta nghỉ [Em] ngơi
-[Dm] Xóa lao [G] nhọc của tháng năm ngược [C] xuôi
-[F] Hỡi bao người đời [C] sống luôn nổi [Em] trôi
-[Dm] Đến bên [G] Ngài rồi sẽ yên vui một [C] đời.
+{
+  id: 1767001582485,
+  title: "Đồi vắng",
+  artist: "Lê Phước Thiện",
+  key: "D",
+  content: `
+Intro: [Dm][Bb]-[Gm][Edim]-[A7][Dm]
+
+1. Đồi [Dm] vắng trong sương chiều, Jêsus [Bb] đã trút hơi [Gm] rồi
+Ngàn cây [Dm] đứng như ru hồn nhân thế đang trầm [A7] luân
+Thập [Dm] giá treo thân người mang bao [Bb] vết gươm căm [Gm] hờn
+Bởi [A7] người không biết chi tình yêu Chúa [Dm] Cha
+
+Vì tình [Gm] yêu nên Thiên Chúa sai Ngài đến [Dm] dương trần
+Gánh [A7] thay bao đớn đau đời người lầm [Dm] than
+Nhưng thiên [C] tình Ngài ban cho, nhân gian đó [F] đã khinh thường
+Thế [A7] nhân vì tội ô lìa xa Chúa [D7], giờ [Gm] đây thân Chúa đau [A7] thương
+
+Đồi [Dm] Gô-gô-tha nhuốm huyết tuôn Jesus phó thân mình vì [Bb] tôi
+[A7] Nằm [Dm] xuống thế cho [A7] người
+Jêsus hỡi cớ sao [Bb] Ngài nhân từ tha thứ cho trần gian hờn [Dm] căm
+
+2. Kìa [Dm] đó chốn an phần nơi thanh [Bb] vắng bóng đêm [Gm] dài
+Ru theo [Dm] tiếng hát êm đềm đưa bước chân biệt [A7] ly
+Đường [Dm] vắng bóng con người đem thân [Bb] Chúa đến nơi [Gm] mộ
+Muôn đời như cách xa [A7] tội ô thế [Dm] nhân
+
+Vì tình [Gm] yêu nên Thiên Chúa sai Ngài đến [Dm] dương trần
+Gánh [A7] thay bao đớn đau đời người lầm [Dm] than
+Nhưng thiên [C] tình Ngài ban cho, nhân gian đó [F] đã khinh thường
+Thế [A7] nhân vì tội ô lìa xa Chúa [D7], giờ [Gm] đây thân Chúa đau [A7] thương
+
+Đồi [Dm] Gô-gô-tha nhuốm huyết tuôn Jesus phó thân mình vì [Bb] tôi
+[A7] Mộ [Dm] Chúa niêm phong rồi Tình yêu đó quá cao [Gm] đẹp
+Muôn [A7] đời cho chính anh và tôi giờ [Dm] đây
   `
 }
 ];
