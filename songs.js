18ab06703a3d94927c59d82e68ba5aac74b9f37a
anphastar2010 <anphastar2010@gmail.com>
2026-05-28 17:23:04 +0700

    Update songs.js

--- songs.js
+++ songs.js
@@ -60,5 +60,97 @@ Thế [A7] nhân vì tội ô lìa xa Chúa [D7], giờ [Gm] đây thân Chúa �
 [A7] Mộ [Dm] Chúa niêm phong rồi Tình yêu đó quá cao [Gm] đẹp
 Muôn [A7] đời cho chính anh và tôi giờ [Dm] đây
   `
+},
+
+{
+  id: 1779961564011,
+  title: "[1] AI SẼ NGỰ TRÊN NÚI THÁNH",
+  artist: "Trịnh Chúc",
+  key: "A",
+  content: `
+Lạy [Am]Chúa ai sẽ trú trên đền tạm Ngài nào ai, Chúa ơi? 
+Và [F]ai sẽ ngự trên núi thánh [E]Ngài nào ai Chúa [Am]ơi? 
+[A]Ấy [Dm]là người bước đi thẳng [E]ngay. Làm điều công [C]chính [A]Làm điều công [Dm]chính [E]và nói thật trong[Am]lòng.
+Lạy Chúa [A]người không cho vay lời [Dm]không làm chuyện bất [E]công để [C]hại người[E]khác, 
+nào [Am]ai tuân giữ điều [Dm]này sẽ không không bao [E]giờ không bao giờ rung [Am]động.
+
+[A]Người có lưỡi không nói [D]hành không làm điều ác[E]gian thiệt hại bạn [A]hữu
+[F#m]Và không lăng [Bm]nhục người [E]lân cận[A]mình
+[D]Mắt khinh [Bm]thường ác [A]gian [E]lòng tin yêu ai nơi [A]Cha
+[F#m]Người thế đã hứa nguyện dẫu rằng dẫu [E]rằng tổn thương cũng không bao [D]giờ [E]đổi[A]thay
+  `
+},
+
+{
+  id: 1779961590654,
+  title: "[2] BÀI CA MỚI",
+  artist: "Trịnh Chúc",
+  key: "D",
+  content: `
+Hãy hát [D]lên, hát [F#m]lên một bài ca [D]mới. 
+Hãy hát [Bm]lên, một [A]bài chúc Đấng Chí [D]Tôn. 
+Giê Su [G]vua, con chúc tụng danh [A]Ngài, 
+từng giờ, từng ngày, từng ngày truyền rao cứu [D]rỗi [A]nhân [D]loại. 
+Hãy truyền [G]rao công việc lạ [Bm]lùng vinh hiển [A]Ngài ngự giữa muôn [D]dân, 
+ngợi khen [G]Chúa Thánh Linh muôn [D]đời. 
+Kính sợ [Em]Người vì Ngài [A]Chúa muôn dân, 
+
+Ngài sẽ [Bm]lấy đức công bằng đến cho muôn [A]người. 
+Ngài lập vững thế gian kia không hề rung [D]động. 
+[D]Nguyện [G]các, từng trời vui, biển [D]sóng, vang ầm sôi, và ruộng [A]đồng, cùng hát tiếng hoan [D]ca. A [E]lê lu [A]ia. 
+[A]Chúa đem công [Em]bình, cảm tạ [G]Chúa hát vang danh [D]Ngài 
+[A]Hát tôn danh [Em]Ngài, Cảm tạ [G]Chúa, hát tôn vinh [D]Ngài.
+  `
+},
+
+{
+  id: 1779961619312,
+  title: "[3] BÀI CA TRÔNG CẬY",
+  artist: "Trịnh Trúc",
+  key: "Am",
+  content: `
+1. [Am]Chúng con [Am]dâng bài [C]ca ngợi ca chí [Am]thánh 
+Chúa muôn vua thần [A]linh trời đất tôn [Dm]thờ 
+Vì Ngài là [C]Giê xu quyền [E]Ngài trong [Dm]vinh quang danh Cha được [E]thánh 
+Kính dâng [Am]Cha đời [C]con Ngài lo sớm [Am]tối Thánh hoá con ngày [A]đêm lòng con mong [Dm]chờ, 
+vì Ngài là [C]Giê xu Tình [E]Ngài luôn [Dm]yêu thương xuống ơn an [Am]bình
+
+Chúa ơi! nguyện [C]xin thần [F]linh toả [A]ơn đời [Dm]con
+Toàn năng Đức Chúa [G]Cha đem con [C]một Ngài đã [Am]ban [G]cho loài người muôn [E]tội
+Chúa [Dm]ơi! lòng [C]con trông cậy [E]Chúa lòng con cảm tạ [E]Chúa Chúa muôn vua quyền [Am]năng.
+
+2. [Am]Dẫu vinh [Am]quang quyền [C]uy mà không có [Am]Chúa
+Dẫu kim cương ngọc [A]trai rồi cũng hoang [Dm]tàn
+Vì đời không [C]có Chúa là [E]đời không [Dm]an vui tâm linh lạnh [E]giá
+Mỗi bước [Am]đi cậy [C]trông tình Cha chí [Am]thánh Nắm tay con Ngài [A]đưa từng bước trong [Dm]đời, 
+vì Ngài là [C]Cứu Chúa Tình [E]Ngài ôi [Dm]bao la yêu thương nhân [Am]loại
+
+Chúa ơi! nguyện [C]xin thần [F]linh toả [A]ơn đời [Dm]con
+Toàn năng Đức Chúa [G]Cha đem con [C]một Ngài đã [Am]ban [G]cho loài người muôn [E]tội
+Chúa [Dm]ơi! lòng [C]con trông cậy [E]Chúa lòng con cảm tạ [E]Chúa Chúa muôn vua quyền [Am]năng.
+  `
+},
+
+{
+  id: 1779962136667,
+  title: "[4] BÌNH AN LÀ CHIẾN THẮNG",
+  artist: "Trịnh Chúc",
+  key: "Em",
+  content: `
+1. [Em]Chúa ơi tâm [G]con không hề đưa cao,
+mắt[B]con không nhìn xa [Em]vời.Tai [C]con không tìm lời [D]ru à [Em]ơi 
+Miệng [G]con không kiêu căng không hề kiêu [Am]căng trong tham [D]danh 
+nhưng chỉ xin [Em]Chúa đem bình [B]an bình an trong Chúa [Em]thôi
+
+[E]Vì một mai kiêu căng rồi cũng sẽ [A]tàn [Am]cũng sẽ [C]tàn, tàn [E]theo năm tháng
+Nhìn cao như nhìn trong [A]gió tham [F#m]vọng nào rồi cũng [E]qua,tham [B]vọng nào rồi cũng [E]qua
+[Em]An vui ôi thật ngọt[Am]ngào an [D]vui ôi thật tuyệt [G]vời, 
+Kìa Chúa đã đem tình [B]yêu Và kìa Chúa đã đem bình [Em]an 
+Bình [G]an đến từ nơi [Am]Chúa bình [D]an đến từ nơi [Em]Chúa
+Là chiến [G]thắng Là an [B]vui cho đời[Em]con
+
+[E]Xin Chúa nắm tay con bằng tình xót thương của [A]Ngài để [B]con lướt qua đường u [E]tối
+Lòng thảnh [B]thơi tìm về bên Chúa như bé yêu trong tay mẹ [Em]hiền
+  `
 }
 ];
